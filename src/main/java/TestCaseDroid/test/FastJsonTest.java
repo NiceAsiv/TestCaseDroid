0db09f2aa00a6ef97ebc2e66a2948f2929938347
Hankoki <fanzixi32@outlook.com>
2024-03-15 20:48:30 +0800

    FastJsonTest类增加两个方法用于测试

--- src/main/java/TestCaseDroid/test/FastJsonTest.java
+++ src/main/java/TestCaseDroid/test/FastJsonTest.java
@@ -12,4 +12,19 @@ public class FastJsonTest {
         JSONObject jsonObject = JSON.parseObject(jsonString);
         System.out.println(jsonObject);
     }
+
+    public void FastJsonTest(){
+        System.out.println("Thanks for call this class!");
+    }
+
+    public void testFunction(){
+        System.out.println("Can I add a new function?");
+        User user = new User(18,"FantaC");
+        String jsonString = JSONObject.toJSONString(user, SerializerFeature.WriteClassName);
+        System.out.println(jsonString);
+        JSONObject jsonObject = JSON.parseObject(jsonString);
+        System.out.println(jsonObject);
+    }
+
+
 }
