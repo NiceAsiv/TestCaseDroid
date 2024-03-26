b26bdca619d018e176f886f583f82ac4349a1791
NiceAsiv <asiwuhe@hotmail.com>
2024-03-26 23:08:36 +0800

    add slf4j backend log4j

--- src/main/java/TestCaseDroid/utils/Neo4jConnectionManager.java
+++ src/main/java/TestCaseDroid/utils/Neo4jConnectionManager.java
@@ -1,11 +1,42 @@
 package TestCaseDroid.utils;
+import lombok.extern.slf4j.Slf4j;
 import org.neo4j.driver.*;
 
+import java.io.InputStream;
+import java.util.Properties;
+
+@Slf4j
 public class Neo4jConnectionManager {
     private Driver driver;
 
     public void connect(String uri, String user, String password) {
         driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+    }
+
+    public void connect() {
+        Properties prop = new Properties();
+        try(InputStream input = getClass().getClassLoader().getResourceAsStream("neo4j.properties")) {
+            if (input == null) {
+                log.error("neo4j.properties not found, please create one in src/main/resources");
+                throw new Exception("neo4j.properties not found, please create one in src/main/resources");
+            }
+
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            //get the property value and print it out
+            String uri = prop.getProperty("uri");
+            String user = prop.getProperty("user");
+            String password = prop.getProperty("password");
+
+            if (uri == null || user == null || password == null) {
+                log.error("uri, user, password must be set in neo4j.properties");
+                throw new Exception("uri, user, password must be set in neo4j.properties");
+            }
+            driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
     public Session getSession() {
@@ -18,7 +49,7 @@ public class Neo4jConnectionManager {
 
     public static void main(String[] args) {
         Neo4jConnectionManager connectionManager = new Neo4jConnectionManager();
-        connectionManager.connect("bolt://localhost:7687", "neo4j", "abc123");
+        connectionManager.connect();
         connectionManager.close();
     }
 }
