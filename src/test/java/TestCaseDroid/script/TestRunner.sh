14a5df2e6579d7b53fc63ed75aa58aa9c7d776ef
NiceAsiv <asiwuhe@hotmail.com>
2024-04-10 11:14:32 +0800

    fix TestCaseRunner

--- src/test/java/TestCaseDroid/script/TestRunner.sh
+++ src/test/java/TestCaseDroid/script/TestRunner.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Define variables
-CLASSPATH="/path/to/classes"
-TEST_CLASS="my.package.MyTest"
-JUNIT_JAR="/path/to/junit-platform-console-standalone.jar"
+#输入提示
+echo "请输入项目路径和测试类名："
+# shellcheck disable=SC2028
+echo "eg:bash TestRunner.sh E:\\Tutorial\\taitest\\ test.CFGTestTest"
+read -r PROJECT_PATH TEST_CLASS_NAME
+#输出输入的路径和类名
+echo "项目路径：$PROJECT_PATH"
+echo "测试类名：$TEST_CLASS_NAME"
 
-# Construct the command
-COMMAND="java -jar $JUNIT_JAR -cp $CLASSPATH --select-class $TEST_CLASS"
 
-# Run the command and print the output
-echo "Running command: $COMMAND"
-$COMMAND
+# Run the test case using Maven
+OUTPUT=$(mvn -f "$PROJECT_PATH" test -Dtest="$TEST_CLASS_NAME")
+
+
+# Parse the test results
+TOTAL_TESTS=$(echo "$OUTPUT" | grep "Tests run:" | awk '{print $3}' | cut -d ',' -f1)
+FAILED_TESTS=$(echo "$OUTPUT" | grep "Tests run:" | awk '{print $6}' | cut -d ',' -f1)
+ERROR_TESTS=$(echo "$OUTPUT" | grep "Tests run:" | awk '{print $8}' | cut -d ',' -f1)
+SKIPPED_TESTS=$(echo "$OUTPUT" | grep "Tests run:" | awk '{print $10}' | cut -d ',' -f1)
+
+# Print the test results
+echo "Total tests: $TOTAL_TESTS"
+echo "Failed tests: $FAILED_TESTS"
+echo "Error tests: $ERROR_TESTS"
+echo "Skipped tests: $SKIPPED_TESTS"
